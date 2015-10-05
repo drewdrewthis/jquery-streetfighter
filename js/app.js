f049c86216bf9a393e7569de1e6e1f5581243975
Andrew Joia <drewdrewthis@gmail.com>
2015-10-05 07:40:53 -0400

    Set up mouseover/leave events

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
-    $('div.ryu-throwing').show();
-    $('div.ryu-still').hide();
-    $('div.hadouken').show();
+    //$('div.ryu-throwing').hide();
+    //$('div.ryu-still').hide();
+    //$('div.hadouken').hide();
+
+    $('.ryu').mouseenter(function() {
+    	console.log('mouse entered .ryu div');
+    	$('.ryu-still').hide();
+		$('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+    	console.log('mouse left');
+    	$('.ryu-ready').hide();
+		$('.ryu-still').show();
+  	});
+
 });
